86417e0b155e62afae92627ec6115cc1a64ad6d4
Егор Тумаков <el.tumakov@gmail.com>
2020-04-28 16:02:31 +0300

    Настраивает автоматизацию сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
 "use strict";
 
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var del = require("del");
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 85, progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+ });
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/spr-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,14 +49,52 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
+gulp.task("cleanScript", function () {
+  return del("build/js/script.js");
+});
+
+gulp.task("script", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(concat("script.js"))
+    .pipe(uglify())
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/*.html",
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+ });
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +102,22 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("source/img/spr-*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("cleanScript", "script"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "cleanScript",
+  "script",
+  "sprite"
+));
+
+gulp.task("start", gulp.series(
+  "build",
+  "server"
+  ));
---
+++ source/js/map.js
@@ -0,0 +1,45 @@
+(function () {
+  var TABLET_WIDTH = 768;
+  var DESKTOP_WIDTH = 1440;
+  var myMap;
+
+  // Дождёмся загрузки API и готовности DOM.
+  ymaps.ready(init);
+
+  function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [59.938635, 30.323118], // Москва
+      zoom: 18,
+      controls: []
+    }),
+
+    placemark = new ymaps.Placemark(myMap.getCenter(), {
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/map-pin.png',
+      // Размеры метки.
+      iconImageSize: [124, 106],
+      iconImageOffset: [-61, -53]
+    });
+
+    if (window.innerWidth < TABLET_WIDTH) {
+      myMap.setZoom(17);
+      placemark.options.set('iconImageSize', [62, 54]);
+      placemark.options.set('iconImageOffset', [-30, -40]);
+    }
+
+    if (window.innerWidth >= DESKTOP_WIDTH) {
+      myMap.setCenter([59.939058, 30.319274], 17);
+      placemark.options.set('iconImageOffset', [-80, -130]);
+    }
+
+    myMap.geoObjects.add(placemark);
+  }
+})();
